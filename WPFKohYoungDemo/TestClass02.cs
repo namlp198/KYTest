74b020b65f159e2ba520ba04186333bb2326d670
DESKTOP-V0FEVG4\namlp <nathan@kohyoung.com>
2022-04-07 10:53:19 +0700

    Add new class: TestClass02

---
+++ WPFKohYoungDemo/TestClass02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFKohYoungDemo
+{
+    public class TestClass02
+    {
+        private string name;
+        public TestClass02(string name)
+        {
+            this.name = name;
+        }
+    }
+}
